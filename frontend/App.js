fa2951e909ec517b5fc8f4ead4f418ca06ce2ab0
Rijan-Stha <shrestharijan056@gmail.com>
2024-03-15 14:29:10 +0545

    setup icoMoon fonts and icons as well as laoded the font

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import { createIconSetFromIcoMoon } from "@expo/vector-icons";
+
+const Icon = createIconSetFromIcoMoon(
+  require("./assets/icons/icomoon/selection.json"),
+  "IcoMoon",
+  "icomoon.ttf"
+);
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    IcoMoon: require("./assets/icons/icomoon/icomoon.ttf"),
+    "Poppins-Black": require("./assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("./assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("./assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("./assets/fonts/Poppins-Thin.ttf"),
+    "DMRegular": require("./assets/fonts/DMSans-Regular.ttf"),
+    "DMMedium": require("./assets/fonts/DMSans-Medium.ttf"),
+    "DMBold": require("./assets/fonts/DMSans-Bold.ttf"),
+  });
+
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Icon name="plus" size={32} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +42,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
